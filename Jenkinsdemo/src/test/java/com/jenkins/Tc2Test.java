de5f807d6d1ac1c261d7da8b60bb9e0e0701228f
Asish154017 <HP@ASISH>
2024-02-16 11:39:17 +0530

    m2 is passed

---
+++ Jenkinsdemo/src/test/java/com/jenkins/Tc2Test.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc2Test 
+{
+	@Test
+	public void m2()
+	{
+		Reporter.log("m2 passed",true);
+	}
+
+}
