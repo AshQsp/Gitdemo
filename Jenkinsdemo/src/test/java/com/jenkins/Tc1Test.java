9f4fab023b620736698c6b4662a2a9646d24a9d7
Asish154017 <HP@ASISH>
2024-02-16 11:38:55 +0530

    m1 is passed

---
+++ Jenkinsdemo/src/test/java/com/jenkins/Tc1Test.java
@@ -0,0 +1,14 @@
+package com.jenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Tc1Test
+{
+	@Test
+	public void m1()
+	{
+		Reporter.log("m1 passed",true);
+	}
+
+}
